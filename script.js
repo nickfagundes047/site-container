0233773bff68aa81b6ceb0b1a33b5da5b4211e6b
nickfagundes047 <nickfagundees@gmail.com>
2026-07-23 19:54:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,47 @@ categorias.forEach(categoria => {
 
         tituloCategoria.innerText = categoria.innerText;
 
-        produtos.innerHTML = `
-            <p>Em breve os produtos desta categoria aparecerão aqui.</p>
-        `;
+       produtos.innerHTML = `
+<div class="produto">
+
+    <h3>Xis Salada</h3>
+
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo muçarela e o recheio escolhido.</p>
+
+    <strong>R$ 25,00</strong><br><br>
+
+    <button>+ Adicionar</button>
+
+</div>
+
+<hr>
+
+<div class="produto">
+
+    <h3>Xis Frango</h3>
+
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo muçarela e o recheio escolhido.</p>
+
+    <strong>R$ 25,00</strong><br><br>
+
+    <button>+ Adicionar</button>
+
+</div>
+
+<hr>
+
+<div class="produto">
+
+    <h3>Xis Calabresa</h3>
+
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo muçarela e o recheio escolhido.</p>
+
+    <strong>R$ 30,00</strong><br><br>
+
+    <button>+ Adicionar</button>
+
+</div>
+`;
 
     });
 
