1b0a3cf9883066dfc4ca62e7ae2a02c064f2d75f
nickfagundes047 <nickfagundees@gmail.com>
2026-07-23 19:11:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const categorias = document.querySelectorAll(".categoria-card");
+const listaProdutos = document.getElementById("lista-produtos");
 
+categorias.forEach(categoria => {
+    categoria.addEventListener("click", () => {
+        listaProdutos.innerHTML = `
+            <h3>🍔 Hambúrgueres Artesanais</h3>
+
+            <p>Em breve os produtos aparecerão aqui.</p>
+        `;
+    });
+});
