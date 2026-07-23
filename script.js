e7a95581040445e70b082c0c45ec402ece444e50
nickfagundes047 <nickfagundees@gmail.com>
2026-07-23 19:34:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const categorias = document.querySelectorAll(".categoria-card");
+
+const areaCategorias = document.getElementById("categorias");
 const listaProdutos = document.getElementById("lista-produtos");
 
-categorias.forEach(categoria => {
-    categoria.addEventListener("click", () => {
-        listaProdutos.innerHTML = `
-            <h3>🍔 Hambúrgueres Artesanais</h3>
+const tituloCategoria = document.getElementById("titulo-categoria");
+const produtos = document.getElementById("produtos");
 
-            <p>Em breve os produtos aparecerão aqui.</p>
+const botaoVoltar = document.getElementById("voltar");
+
+categorias.forEach(categoria => {
+
+    categoria.addEventListener("click", () => {
+
+        areaCategorias.style.display = "none";
+
+        listaProdutos.style.display = "block";
+
+        tituloCategoria.innerText = categoria.innerText;
+
+        produtos.innerHTML = `
+            <p>Em breve os produtos desta categoria aparecerão aqui.</p>
         `;
+
     });
+
+});
+
+botaoVoltar.addEventListener("click", () => {
+
+    listaProdutos.style.display = "none";
+
+    areaCategorias.style.display = "flex";
+
 });
