d4cb860c5e9383df0df5e27e8fe7e9ba00516153
nickfagundes047 <nickfagundees@gmail.com>
2026-07-23 20:43:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ categorias.forEach(categoria => {
 
     <h3>Xis Salada</h3>
 
-    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo muçarela e o recheio escolhido.</p>
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo mussarela e o hamburguer.</p>
 
     <strong>R$ 25,00</strong><br><br>
 
@@ -37,7 +37,7 @@ categorias.forEach(categoria => {
 
     <h3>Xis Frango</h3>
 
-    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo muçarela e o recheio escolhido.</p>
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo mussarela e o frango 150g.</p>
 
     <strong>R$ 25,00</strong><br><br>
 
@@ -51,9 +51,51 @@ categorias.forEach(categoria => {
 
     <h3>Xis Calabresa</h3>
 
-    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo muçarela e o recheio escolhido.</p>
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo mussarela e a calabresa 150g.</p>
 
     <strong>R$ 30,00</strong><br><br>
+
+    <button>+ Adicionar</button>
+
+</div>
+
+<hr>
+
+<div class="produto">
+
+    <h3>Xis Coração</h3>
+
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo mussarela e coração 150g.</p>
+
+    <strong>R$ 32,00</strong><br><br>
+
+    <button>+ Adicionar</button>
+
+</div>
+
+<hr>
+
+<div class="produto">
+
+    <h3>Xis Bacon</h3>
+
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo mussarela, hamburguer e bacon.</p>
+
+    <strong>R$ 34,00</strong><br><br>
+
+    <button>+ Adicionar</button>
+
+</div>
+
+<hr>
+
+<div class="produto">
+
+    <h3>Xis Acebolado</h3>
+
+    <p>Pão de 16 cm, maionese caseira, milho, ervilha, alface, tomate, ovo, queijo mussarela e hamburguer com cebola.</p>
+
+    <strong>R$ 27,00</strong><br><br>
 
     <button>+ Adicionar</button>
 
